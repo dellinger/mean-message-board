704578acc663870732cab8f360764c0306d6aa56
David Ellinger <david.ellinger@me.com>
2016-08-27 00:51:45 -0700

    Added control to form

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,7 +1,21 @@
 /// <reference path="../../typings/index.d.ts" />
 
+class MainController {
+  constructor() {
+    'ngInject';
+  }
+
+  public postMessage() {
+    console.log('post');
+  }
+
+}
+
+
 angular
   .module('app')
   .component('app', {
-    templateUrl: 'app/main.html'
+    templateUrl: 'app/main.html',
+    controller: MainController,
+    controllerAs: 'main'
   });
